4be939d15d091104d03dc8fca8abb5568b1152fe
unknown <michaelcmatteis@gmail.com>
2024-12-29 15:12:19 -0600

    adding

---
+++ Controllers/TaskItemsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using taskMangerment.Models;
+
+namespace taskMangerment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskItemsController : ControllerBase
+    {
+        private static List<TaskItem> TaskItmes = new List<TaskItem>() 
+        {
+            new TaskItem{ id =1, Title = "Website Redesign", Description = "redsigning test"},
+            new TaskItem{ id =2, Title = "test", Description = "test"}
+        };
+
+        [HttpGet]
+        public IEnumerable<TaskItem> Get() 
+        {
+            return TaskItmes;
+        }
+    }
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,11 @@
+﻿namespace taskMangerment.Models
+{
+    public class TaskItem
+    {
+        public int id{ get; set; }
+
+        public string Title{ get; set; }
+        public string Description {  get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now; 
+    }
+}
